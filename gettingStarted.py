a52c7b510ea7c7bca5f610be447e272e21d3e9b6
sank2024 <sk11321@nyu.edu>
2024-01-29 16:56:36 -0500

    lATEST_COMMIT_d

--- gettingStarted.py
+++ gettingStarted.py
@@ -10,7 +10,7 @@ def welcome_assignment_answers(question):
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = 4
+        answer = 7
     elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "pcap"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
