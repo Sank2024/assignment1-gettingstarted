903df00294e4563006371a02d33834f54a9677d3
sank2024 <sk11321@nyu.edu>
2024-01-29 17:02:34 -0500

    lATEST_COMMIT_d

--- gettingStarted.py
+++ gettingStarted.py
@@ -22,11 +22,11 @@ def welcome_assignment_answers(question):
     elif question =="Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = 2
+        answer = 3
 
     else:
-        answer = "This is not the right answer"
-    return (answer)
+        answer = "This is not the answer you're looking for."
+    return answer
 
 
 # Complete all the questions.
