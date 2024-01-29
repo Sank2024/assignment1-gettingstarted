0a50f6cd47c2afc5f11ba33cff7a8299f28d54f8
sank2024 <sk11321@nyu.edu>
2024-01-29 16:51:51 -0500

    lATEST_COMMIT_d

--- gettingStarted.py
+++ gettingStarted.py
@@ -10,7 +10,7 @@ def welcome_assignment_answers(question):
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = "4"
+        answer = 4
     elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "pcap"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
@@ -22,7 +22,7 @@ def welcome_assignment_answers(question):
     elif question =="Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = int(3)
+        answer = 3
 
     else:
         answer = "This is not the right answer"
