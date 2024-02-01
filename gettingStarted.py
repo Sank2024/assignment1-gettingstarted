10224a3b2e1b436e9cea2669b0797709d4e323ce
sank2024 <sk11321@nyu.edu>
2024-02-01 13:54:19 -0500

    lATEST_COMMIT_E

--- gettingStarted.py
+++ gettingStarted.py
@@ -6,27 +6,27 @@ def welcome_assignment_answers(question):
     # Students do not have to follow the skeleton for this assignment.
     # Another way to implement is using a "case" statements similar to C.
 
-    if question == "Are encoding and encryption the same? - Yes/No":
+    if question.strip() == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+    elif question.strip() == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
-    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+    elif question.strip() == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
         answer = 5
-    elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+    elif question.strip() == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "pcap"
-    elif question == "Is it possible to decode a message without a key? - Yes/No":
+    elif question.strip()== "Is it possible to decode a message without a key? - Yes/No":
         answer = "Yes"
-    elif question =="Is a hashed message supposed to be un-hashed? - Yes/No":
+    elif question.strip() =="Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
-    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code -":
+    elif question.strip() == "What is the SHA256 hashing value of your NYU email and use the answer in your code -":
         answer = "c504dc6362e24f920b065bbb5c5249104108ae530bd47b573213db898e2177fc"
-    elif question =="Is MD5 a secured hashing algorithm? - Yes/No":
+    elif question.strip() =="Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
-    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+    elif question.strip() == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         answer = 3
     else:
         question = question.strip()
-        answer = f"Invalid question: '{question}' does not match any predefined questions"
+        answer = f"'{question.strip()}' does not match any predefined questions."
     return answer
 
 # Complete all the questions.
